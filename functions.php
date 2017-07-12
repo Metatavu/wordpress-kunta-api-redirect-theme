14bd71a7d70be0e77b74930ddc4207a6c249bd45
Heikki Kurhinen <heikki.kurhinen@metatavu.fi>
2017-07-12 14:34:20 +0300

    Added menu support

--- functions.php
+++ functions.php
@@ -106,9 +106,10 @@
     
   });
   
-  add_action('after_setup_theme', function () {	
+  add_action('after_setup_theme', function () {
+	add_theme_support( 'menus' );
     add_theme_support( 'post-thumbnails' );
-	  set_post_thumbnail_size( 1200, 9999 );
+	set_post_thumbnail_size( 1200, 9999 );
   });
   
 ?>
